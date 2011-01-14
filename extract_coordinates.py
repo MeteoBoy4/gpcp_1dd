b9f621b9d63b8bb062bad82645a94430f332d8a7
Connor McCoy <connor.mccoy@seattlebiomed.org>
2011-01-14 13:58:25 -0800

    Updated extract_coordinates.py to use onedd.py, data_iter

--- extract_coordinates.py
+++ extract_coordinates.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+"""
+Simple utility to read GPCP 1DD file, outputting measurements
+for provided coordinates.
+"""
 
 import argparse
 import contextlib
@@ -6,38 +10,33 @@ import csv
 import glob
 import sys
 
-import gpcp
+import onedd
 
 def main(args=sys.argv[1:]):
-    parser = argparse.ArgumentParser()
+    """
+    Parses arguments, reads file(s), prints results to output
+    """
+    parser = argparse.ArgumentParser(description="""GPCP 1DD Extraction utility -
+filters the file(s) specified by the provided coordinates.""")
     parser.add_argument('latitude', type=float)
     parser.add_argument('longitude', type=float)
     parser.add_argument('file', nargs="+")
-    parser.add_argument('-o', '--output')
+    parser.add_argument('-o', '--output', type=argparse.FileType('w'),
+                        default=sys.stdout,
+                        help="Output file (default: stdout)")
 
     args = parser.parse_args()
 
-    if args.output is None:
-        output = sys.stdout
-    else: 
-        output = open(args.output, 'w')
-
-    writer = csv.writer(output)
+    writer = csv.writer(args.output)
     writer.writerow(('year', 'month', 'day', 'lat', 'lon', 'value'))
-    
-    for path in args.file:
-        fp = open(path)
-        with contextlib.closing(fp):
-            data_file = gpcp.GPCPDataFile(fp)
-            data_file.load_data()
-            day_data = data_file.day_data
-            for day in day_data:
-                data = ((data_file.year, data_file.month,
-                         day.day, lat, lon, value) 
-                        for (lat, lon), value in day
-                        if lat == args.latitude and lon == args.longitude)
+    with contextlib.closing(args.output):
+        for path in args.file:
+            fp = open(path)
+            with contextlib.closing(fp):
+                data_file = onedd.reader(fp)
+                data = (i for i in data_file.data_iter()
+                        if i[1] == args.latitude and i[2] == args.longitude)
                 writer.writerows(data)
-    output.close()
 
 if __name__ == '__main__':
     main()
