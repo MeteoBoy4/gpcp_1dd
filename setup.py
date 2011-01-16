a3ad80db2cb12489dc68d7ee3dab7fa36a411565
Connor McCoy <connorm@gmail.com>
2011-01-16 15:58:40 -0800

    Tweaked setup

--- setup.py
+++ setup.py
@@ -6,8 +6,9 @@ setup(name='gpcp',
       author='Connor McCoy <connormccoy@gmail.com>',
       version='2011-01-14',
       description='Tools for working with GPCP 1DD precipitation data.',
-      package_dir={'gpcp': '.'},
+      package_dir={'gpcp': 'gpcp'},
       packages=['gpcp'],
-      scripts=['scripts/read_onedd_headers.py', 'scripts/onedd_to_delim.py'],
+      scripts=['gpcp/scripts/read_onedd_headers.py',
+               'gpcp/scripts/onedd_to_delim.py'],
       requires=['argparse']
       )
